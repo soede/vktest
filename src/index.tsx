a7741a89a8412addd77a93b2c470865f53498248
adam <69597083+Yournam-e@users.noreply.github.com>
2024-04-25 20:27:33 +0300

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import {App} from "./App";
+import {router} from "./ui/layout/router";
+import {
+    RouterProvider,
+} from "react-router-dom";
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+      <RouterProvider router={router} />
+  </React.StrictMode>
+);
+
---
+++ src/ui/layout/router.tsx
@@ -0,0 +1,9 @@
+import {createBrowserRouter} from "react-router-dom";
+import React from "react";
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <div>Hello world!</div>,
+    },
+]);
